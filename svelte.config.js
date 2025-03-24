098bc1fb756e66eda3713ef69e065ac2a75146e0
Martial59110 <floquet.martial@gmail.com>
2025-03-24 23:26:23 +0100

    Fix: reconfiguration complète pour GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -12,12 +12,16 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
 			fallback: 'index.html',
+			precompress: false,
 			strict: false
 		}),
 		paths: {
-			base: '/Martial59110.github.io'
-		}
+			base: ''
+		},
+		appDir: 'app'
 	}
 };
 
