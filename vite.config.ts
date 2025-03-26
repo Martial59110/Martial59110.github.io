a1fb3156c88033c96b39a47298babcbb736f87f8
Martial59110 <floquet.martial@gmail.com>
2025-03-26 11:11:20 +0100

    Fix: correction des chemins Bootstrap et ajout des fichiers dans vendor

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	build: {
+		rollupOptions: {
+			external: [
+				'/node_modules/bootstrap/dist/css/bootstrap.min.css',
+				'/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+			]
+		}
+	}
 });
